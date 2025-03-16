9ddbf852f8713601b998b7f9779e0925517cb42f
¨kubraaygun¨ <kubraaygun@outlook.com.tr>
2025-03-16 23:04:38 +0300

    update ios

--- .history/ios/notemaps/AppDelegate_20250316225513.mm
+++ .history/ios/notemaps/AppDelegate_20250316225513.mm
@@ -6,7 +6,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  [GMSServices provideAPIKey:@"AIzaSyAgHVb_bcjIUlrZN8NVP0meMUBR6YojmP8"];
+  [GMSServices provideAPIKey:@"_YOUR_API_KEY_"];
   self.moduleName = @"notemaps";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
