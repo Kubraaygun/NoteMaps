8ad72a66bbfadb0444c0a3df91d382f3edd8f15f
¨kubraaygun¨ <kubraaygun@outlook.com.tr>
2025-03-17 21:15:02 +0300

    Add mapExample

--- ios/notemaps/AppDelegate.mm
+++ ios/notemaps/AppDelegate.mm
@@ -1,13 +1,15 @@
 #import "AppDelegate.h"
 #import <GoogleMaps/GoogleMaps.h>
 #import <React/RCTBundleURLProvider.h>
-
+#import "ReactNativeConfig.h"
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  [GMSServices provideAPIKey:@""];
-  self.moduleName = @"notemaps";
+  NSString *googleApiKey = [ReactNativeConfig envFor:@"GOOGLE_API_KEY"];
+  [GMSServices provideAPIKey:googleApiKey];
+
+
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
