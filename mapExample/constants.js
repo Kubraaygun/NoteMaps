43e23c14ee3945272de753bd91722316b26bd191
¨kubraaygun¨ <kubraaygun@outlook.com.tr>
2025-03-19 01:38:41 +0300

    update

---
+++ mapExample/constants.js
@@ -0,0 +1,10 @@
+export const MapTypes = {
+  STANDARD: 'standard',
+  SATELLITE: 'satellite',
+  HYBRID: 'hybrid',
+  TERRAIN: 'terrain',
+  NONE: 'none',
+  MUTEDSTANDARD: 'mutedStandard',
+  SATELLITE_FLYOVER: 'satelliteFlyover',
+  HYBRID_FLYOVER: 'hybridFlyover',
+};
